1b68c6c861f89474eb292fbe086269d6b7c0afe7
Flabby <flabby@orionlife.net>
2018-07-27 19:39:53 -0400

    API Info

--- core/app.js
+++ core/app.js
@@ -68,7 +68,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const APIKey = config.APIKey;
+const APIKey = config.API;
 app.use((req, res, next) => {
     req.APIKey = APIKey;
     req.isOnlyLetters = function(text) {
